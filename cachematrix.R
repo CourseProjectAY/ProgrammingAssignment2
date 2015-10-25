bf4c36cd3a437e3a5eee135ab03e6914cdddd8e3
Alok Yadav <alokyadav2004@gmail.com>
2015-10-25 12:11:01 +0530

    changes made to the cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -13,7 +13,15 @@ makeCacheMatrix <- function(x = matrix()) {
              setinverse = setinverse,
              getinverse = getinverse)
 }
+## This function cachesolve computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## above. if the inverse has already been calculated and if the matrix has not changed, 
+## then the cachesolve should retrieve the inverse from the cache
 
+## The function calculates the inverse of the special "matrix" created with the 
+## above function makeCacheMatrix. However, it first checks to see if the inverse has already 
+## been calculated. if so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the data and sets the value of the mean in the cache
+##  via the setinverse function.
 
 cachesolve <- function(x, ...) {
         inver <- x$getinverse()
